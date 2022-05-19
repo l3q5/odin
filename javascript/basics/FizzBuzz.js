fa574be95737661f1043869a2e280a2edff98db9
l3q5 <l3q5@protonmail.com>
2022-05-19 17:24:18 -0300

    Finished FizzBuzz in javascript.

---
+++ javascript/basics/FizzBuzz.js
@@ -0,0 +1,17 @@
+let num = parseInt(prompt("Enter the desired number: "));
+for (let i = 1; i < num; i++){
+    switch(true){
+        case (i % 3 == 0 && i % 5 == 0):
+            console.log("FizzBuzz");
+            break;
+        case (i % 3 == 0):
+            console.log("Fizz");
+            break;
+        case (i % 5 == 0):
+            console.log("Buzz");
+            break;
+        default:
+            console.log(i);
+            break;
+    }
+}
